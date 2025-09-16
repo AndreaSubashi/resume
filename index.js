d1d53d6f0e54dfcb5f8a5c04faeb7e45ab7f5087
Andrea Subashi <105102517+AndreaSubashi@users.noreply.github.com>
2025-09-16 22:19:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let modal = document.getElementsByClassName("modal")[0]
 let img = document.getElementById("profile")
 let modalImg = document.getElementsByClassName("modal_content")[0]
 let captionText = document.getElementById("caption")
+let phone = document.getElementById("phone")
 
 img.onclick = function(){
     modal.style.display = "block"
@@ -14,3 +15,13 @@ let span = document.getElementsByClassName("close")[0]
 span.onclick = function() {
     modal.style.display = "none"
 }
+
+document.querySelectorAll(".copy_btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    navigator.clipboard.writeText(btn.dataset.copy).then(() => {
+      btn.textContent = "Copied!";
+      setTimeout(() => btn.textContent = "Copy", 3500);
+    });
+  });
+});
+
